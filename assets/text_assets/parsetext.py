1e227dc52fe2c4a319458ad8154100a37601d0be
Harry Law <harryalaw@gmail.com>
2021-01-19 14:22:26 +0000

    Made all words start with capital letters

--- assets/text_assets/parsetext.py
+++ assets/text_assets/parsetext.py
@@ -1,10 +1,12 @@
 # Python script which translates my list of text prompts to json format
+import string
+
 with open("text.txt") as f:
     read_data = f.read()
     read_data = read_data.split("\n")
     read_data = [prompt.split(":") for prompt in read_data]
     read_data = [
-        f"[\"{pair[0]}\",\"{pair[1]}\"]" for pair in read_data]
+        f"[\"{string.capwords(pair[0])}\",\"{string.capwords(pair[1])}\"]" for pair in read_data]
 
 with open("prompts.json", "w") as f:
     f.write("{\n\t\"prompts\" : [\n\t\t")
