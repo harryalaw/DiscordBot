267d16c27f3966f1b26f2276ffb6afeaca7fef05
Harry Law <harryalaw@gmail.com>
2021-01-30 15:40:54 +0000

    Sends a dm on join, to make sure that DMs are already set up

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,4 +1,5 @@
-const Util = require('../utility/Util.js')
+const Util = require('../utility/Util.js');
+const { prefix } = require('./../../config.json');
 
 module.exports = {
     name: 'join',
@@ -11,7 +12,16 @@ module.exports = {
         const { channel, member } = message;
         let game = games.get(channel.id);
         if (game.players.has(member)) return;
-        game.addPlayer(member, args[0]);
-        channel.send(`Added ${Util.getName(member)} to Team ${game.players.get(member) + 1}`);
+        // Check that a user has DMs allowed and send helpful message.
+        message.author.send(`If you need any help with the rules of the game or how to use me type \`${prefix}help\` or \`${prefix}rules\`.`)
+            .then(() => {
+                game.addPlayer(member, args[0]);
+                return channel.send(`Added ${Util.getName(member)} to Team ${game.players.get(member) + 1}`);
+            })
+            .catch(error => {
+                console.error(`Could not send join DM to ${message.author.tag}.`);
+                return message.reply(`I couldn't send you a DM, please make sure they are enabled on this server by changing you privacy settings in the top left. Once you've done that type \`${prefix}join\` again!`)
+            })
+
     }
 }
